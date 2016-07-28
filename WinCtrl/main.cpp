a25fcd18a00039fbbd989ff120bfe4505458c315
Soufiane El Aissaoui <elaisoufiane@gmail.com>
2016-07-28 12:00:53 +0100

    Added int handle

--- WinCtrl/main.cpp
+++ WinCtrl/main.cpp
@@ -9,7 +9,7 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 DWORD WINAPI HookThreadFunc(LPVOID);
 HHOOK hhk;
-HWND targetHandle,hLabel, hButton;
+HWND targetHandle, hHexHandlerLabel, hIntHandlerLabel, hCaptureButton;
 int interceptIsOn = 0;
 _declspec(dllexport) LRESULT CALLBACK MouseEvent(int Code, WPARAM wParam, LPARAM lParam) {
 	
@@ -21,20 +21,22 @@ _declspec(dllexport) LRESULT CALLBACK MouseEvent(int Code, WPARAM wParam, LPARAM
 			if (interceptIsOn == 1) {
 				MSLLHOOKSTRUCT mouse = *((MSLLHOOKSTRUCT*)lParam);
 				HWND targetHandle = WindowFromPoint(mouse.pt);
-				char strHandle[64];
-				sprintf_s(strHandle, "%p", targetHandle);
-				SetWindowTextA(hLabel, strHandle);
+				char strHexHandle[64], strIntHandle[64];
+				sprintf_s(strHexHandle, "%p", targetHandle);
+				sprintf_s(strIntHandle, "%d", targetHandle);
+				SetWindowTextA(hHexHandlerLabel, strHexHandle);
+				SetWindowTextA(hIntHandlerLabel, strIntHandle);
 			}			
 		}
 			break;
 		case WM_LBUTTONUP:
-			EnableWindow(hButton, true);
+			EnableWindow(hCaptureButton, true);
 			interceptIsOn = 0;
 			break;
 		case WM_LBUTTONDOWN:
-			if (WindowFromPoint((*((MSLLHOOKSTRUCT*)lParam)).pt) == hButton){
+			if (WindowFromPoint((*((MSLLHOOKSTRUCT*)lParam)).pt) == hCaptureButton){
 				interceptIsOn = 1;
-				EnableWindow(hButton,false);
+				EnableWindow(hCaptureButton,false);
 			}
 			break;
 		default:
@@ -66,7 +68,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, LPSTR lpCmdLine,
 	if (!RegisterClassEx(&wc)) {
 		MessageBox(NULL, "Error registering", "Error", MB_ICONEXCLAMATION | MB_OK);
 	}
-	hwnd = CreateWindowEx(WS_EX_APPWINDOW | WS_EX_TOPMOST, "FirstWnd", "Window Controller", WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX, 500, 100, 340, 420, NULL, NULL, hInstance, NULL);
+	hwnd = CreateWindowEx(WS_EX_APPWINDOW | WS_EX_TOPMOST, "FirstWnd", "Window Controller", WS_OVERLAPPEDWINDOW^WS_THICKFRAME^WS_MAXIMIZEBOX, 500, 100, 345, 420, NULL, NULL, hInstance, NULL);
 
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
@@ -88,11 +90,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam){
 		hookThread = CreateThread(0, 0, HookThreadFunc, NULL, NULL,&dwThread);
 
 
-		hLabel = CreateWindowEx(WS_EX_STATICEDGE, "STATIC", "Handle", WS_CHILD | WS_VISIBLE |SS_CENTER, 241, 80, 80, 25, hwnd, (HMENU)123, NULL, NULL);
-		hButton = CreateWindowEx(WS_EX_STATICEDGE, "BUTTON", "Capture window", WS_CHILD | WS_VISIBLE , 87, 80, 154, 25, hwnd, (HMENU)hookButton, NULL, NULL);
-		if (hLabel==NULL) {
-			MessageBox(NULL, "Error registering", "Error", MB_ICONEXCLAMATION | MB_OK);
-		}
+		hHexHandlerLabel = CreateWindowEx(WS_EX_STATICEDGE, "STATIC", "Hex Handle", WS_CHILD | WS_VISIBLE |SS_CENTER, 7, 80, 80, 25, hwnd, (HMENU)123, NULL, NULL);
+		hIntHandlerLabel = CreateWindowEx(WS_EX_STATICEDGE, "STATIC", "Int Handle", WS_CHILD | WS_VISIBLE | SS_CENTER, 241, 80, 80, 25, hwnd, (HMENU)124, NULL, NULL);
+		hCaptureButton = CreateWindowEx(WS_EX_STATICEDGE, "BUTTON", "Capture window", WS_CHILD | WS_VISIBLE, 87, 80, 154, 25, hwnd, (HMENU)hookButton, NULL, NULL);
 		break;
 	}
 	case WM_CLOSE:
